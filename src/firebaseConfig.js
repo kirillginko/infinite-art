61099b15fbbe5edd441b44e4a02c417676812d02
Kirill Ginko <kirillginko@gmail.com>
2022-01-01 22:38:19 -0500

    added env file

--- src/firebaseConfig.js
+++ src/firebaseConfig.js
@@ -1,13 +1,13 @@
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "@firebase/firestore";
 const firebaseConfig = {
-  apiKey: "AIzaSyA3kDVYEI-DMpbipjCqr7N_-n_3jcs1RBU",
-  authDomain: "art-infinite-3abd8.firebaseapp.com",
-  projectId: "art-infinite-3abd8",
-  storageBucket: "art-infinite-3abd8.appspot.com",
-  messagingSenderId: "869425653198",
-  appId: "1:869425653198:web:64a4b0af56676b2320379f",
-  measurementId: "G-L6ZYZX8CVT",
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID,
 };
 
 const app = initializeApp(firebaseConfig);
